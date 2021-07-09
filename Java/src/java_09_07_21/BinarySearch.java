1530a4a8b6443a3d138f721ca516e7292ade1aa6
Bleyday <46052824+Bleyday@users.noreply.github.com>
2021-07-09 12:11:33 +0200

    Vorlesung vom 09.07.2021

---
+++ Java/src/java_09_07_21/BinarySearch.java
@@ -0,0 +1,11 @@
+package java_09_07_21;
+
+public class BinarySearch {
+    public static void main(String[] args){
+
+    }
+    public static int sort(int [] unsorted_list){
+
+        return 0;
+    }
+}
---
+++ Java/src/java_09_07_21/BubbleSort.java
@@ -0,0 +1,11 @@
+package java_09_07_21;
+
+public class BubbleSort {
+    public static void main(String[] args){
+
+    }
+    public static int sort(int [] unsorted_list){
+
+        return 0;
+    }
+}
