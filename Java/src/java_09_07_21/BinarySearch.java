6df928b22bb7c0a9a840cfba69be4088240f7899
Bleyday <46052824+Bleyday@users.noreply.github.com>
2021-07-15 20:06:06 +0200

    Hausaufgabe BubbleSort.java und BinarySearch.java

--- Java/src/java_09_07_21/BinarySearch.java
+++ Java/src/java_09_07_21/BinarySearch.java
@@ -2,10 +2,30 @@ package java_09_07_21;
 
 public class BinarySearch {
     public static void main(String[] args){
+        int [] menge = {3, 21, 43, 54, 64, 88, 421,6253};
+
+        int position = search(menge, 64);
+        System.out.println("Die gesuchte Nummer ist an der Position " + position);
 
     }
-    public static int sort(int [] unsorted_list){
+    public static int search(int [] list,  int numbr){
+        int u = 0;
+        int o = list.length;
 
-        return 0;
+        while (u <= o){
+             int middle = (u+o) / 2;
+
+             if (list[middle] == numbr){
+                 return middle+1;
+             }
+             else if (numbr < list[middle]){
+                 o = middle -1;
+             }
+             else{
+                 u = middle +1;
+             }
+        }
+
+        return -1;
     }
 }
--- Java/src/java_09_07_21/BubbleSort.java
+++ Java/src/java_09_07_21/BubbleSort.java
@@ -1,11 +1,27 @@
 package java_09_07_21;
 
 public class BubbleSort {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int[] zahlen = {11, 7, 90, 16, 18, 49, 1};
+        int[] sortiert = sort(zahlen);
+
+        for (int i = 0; i < sortiert.length; i++) {
+            System.out.println(sortiert[i]);
+        }
 
     }
-    public static int sort(int [] unsorted_list){
 
-        return 0;
+    public static int[] sort(int[] unsorted_list) {
+        int hilf = 0;
+
+        for (int i = 0; i < unsorted_list.length - 1; i++) {
+            if (unsorted_list[i] > unsorted_list[i + 1]) {
+                hilf = unsorted_list[i];
+                unsorted_list[i] = unsorted_list[i + 1];
+                unsorted_list[i + 1] = hilf;
+                sort(unsorted_list);
+            }
+        }
+        return unsorted_list;
     }
 }
