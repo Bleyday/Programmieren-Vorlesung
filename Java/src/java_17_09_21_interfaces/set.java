ca21fef0f68510d5c8fc817862c71d8e854f239a
peirui <peirui.z@gmx.de>
2021-09-17 13:49:05 +0200

    interfaces

---
+++ Java/src/java_17_09_21_interfaces/set.java
@@ -0,0 +1,8 @@
+package java_17_09_21_interfaces;
+
+public interface set {
+    void add(Object o);
+    void remove (Object o);
+    boolean contains (Object o);
+    boolean isEmpty (Object o);
+}
